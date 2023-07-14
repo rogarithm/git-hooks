f4aedb26b29eac670a0a2278b716d2ed4bd52af4
sehun-kim <rogarithm@gmail.com>
2023-07-15 01:05:41 +0900

    메시지 출력 형식을 다듬는다
     - 최근 커밋과 기존 커밋 메시지 사이에 생기는 빈 줄을 없앴다.
     - 커밋 메시지 형식에 맞춰 '#xxx' -> '# xxx'로 메시지를 출력한다.
     - 여러 역할을 하는 메서드를 나누고, 테스트를 추가했다.

--- prepare-commit-msg/show-recent-commig-msgs.rb
+++ prepare-commit-msg/show-recent-commig-msgs.rb
@@ -4,29 +4,40 @@ $commit_msg_file=ARGV[0]
 $commit_source=ARGV[1]
 $sha1=ARGV[2]
 
-def file_prepend(file, str)
+def print_args
+  puts "commit_msg_file is #{$commit_msg_file}" #.git/COMMIT_EDITMSG
+  puts "commit_source is #{$commit_source}"
+  puts "sha1 is #{$sha1}"
+end
+
+def format_msgs(commit_num, msgs)
+  result = "\n"
+  if msgs.size >= commit_num
+    top_commits = msgs.first(commit_num)
+    top_commits.each_with_index do |msg, index|
+      result << "# #{msg}"
+      result << "\n" unless index == commit_num - 1
+    end
+  end
+  result
+end
+
+def prepend_string(file, str)
   new_contents = ""
   File.open(file, "r") do |file|
     contents = file.read
-    new_contents = "##{str}\n" << contents
+    new_contents = "#{str}" << contents
   end
   File.open(file, "w") do |file|
     file.write(new_contents)
   end
 end
 
-def show_recent_commit_msgs
-  msgs = `git log --pretty=format:'%s'`.split("\n")
-
-  if msgs.size >= 3
-    top_three = msgs.first(3)
-    top_three.each do |msg|
-      file_prepend($commit_msg_file, msg)
-    end
-  end
-
+def show_recent_commit_msgs(commit_num, msgs)
+  result = format_msgs(commit_num, msgs)
+  prepend_string($commit_msg_file, result)
   exit 0
 end
 
-show_recent_commit_msgs
-
+#print_args
+show_recent_commit_msgs(3, `git log --pretty=format:'%s'`.split("\n"))
