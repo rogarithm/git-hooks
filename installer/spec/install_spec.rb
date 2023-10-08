37e87f4d10586c24bbd35b5881c93bf6730d5c34
sehoon.gim <sehoongim@gmail.com>
2023-10-08 19:27:03 +0900

    변수를 메서드 호출로 바꾼다

--- installer/spec/install_spec.rb
+++ installer/spec/install_spec.rb
@@ -8,7 +8,6 @@ describe "Installer", "operations" do
     @install_root_dir='/tmp/test_install_git_hook'
     @install_root_dir2='/tmp/test_install_git_hook2'
     @install_path_list='./install_list'
-    @install_paths=[@install_root_dir, @install_root_dir2]
     @install_full_dir='/tmp/test_install_git_hook/.git/hooks/pre-push'
     @install_full_dir2='/tmp/test_install_git_hook2/.git/hooks/pre-push'
   end
@@ -29,7 +28,7 @@ describe "Installer", "operations" do
   end
 
   it "훅을 설치할 여러 저장소 경로와 발동 조건으로 저장소별 훅 설치 위치를 계산할 수 있다" do
-    @installer.find_install_locations(@trigger_point, @install_paths).should == [File.expand_path(@install_full_dir), File.expand_path(@install_full_dir2)]
+    @installer.find_install_locations(@trigger_point, @installer.prepare_paths_to_install()).should == [File.expand_path(@install_full_dir), File.expand_path(@install_full_dir2)]
   end
   it "한 번에 두 곳에 훅을 설치할 수 있다" do
     @installer.install_hook(@trigger_point, [@installer.find_install_location(@trigger_point, @install_root_dir), @installer.find_install_location(@trigger_point, @install_root_dir2)])
