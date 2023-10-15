7a05e09fcf2e201ff5d99034ef217dd1e89a380d
sehoon.gim <sehoongim@gmail.com>
2023-10-15 19:18:08 +0900

    한 번만 쓰는 값을 인라인한다

--- installer/spec/install_spec.rb
+++ installer/spec/install_spec.rb
@@ -5,8 +5,6 @@ describe "Installer", "operations" do
   before(:each) do
     @installer=Installer.new
     @trigger_point='pre-push'
-    @install_root_dir='/tmp/test_install_git_hook'
-    @install_root_dir2='/tmp/test_install_git_hook2'
     @install_full_dir='/tmp/test_install_git_hook/.git/hooks/pre-push'
     @install_full_dir2='/tmp/test_install_git_hook2/.git/hooks/pre-push'
   end
@@ -56,6 +54,6 @@ describe "Installer", "operations" do
   end
 
   it "훅을 설치할 경로와 발동 조건 정보를 파일에서 읽어올 수 있다" do
-    @installer.prepare_paths_to_install('./spec/data/install_list').should == [[@install_root_dir, @trigger_point], [@install_root_dir2, @trigger_point]]
+    @installer.prepare_paths_to_install('./spec/data/install_list').should == [['/tmp/test_install_git_hook', @trigger_point], ['/tmp/test_install_git_hook2', @trigger_point]]
   end
 end
